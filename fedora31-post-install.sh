c83fbe4477172e130bd4c9d03c3913e4028ac0a0
petescode <mrpmwood@gmail.com>
2019-12-29 11:39:42 -0500

    add vscode repo, proprietary array, log messages

--- fedora31-post-install.sh
+++ fedora31-post-install.sh
@@ -4,15 +4,12 @@ Notes:
     - nVidia installation assumes non-legacy hardware
 
 DEVELOPMENT:
-    - should we use different package arrays for packages from default repos, tainted repos, proprietary, etc?
     - try using "dnf shell" to run all dnf commands at once
     - work on GNOME settings
     - add 3rd party repos
     - add PS1 variable
-    - clear screen
-    - total time ran calculator
-    - echo to screen where the log file is?
     - move logfile to /var/log/
+    - add firmware to one-liner install
 '
 
 if [[ $(id -u) -ne 0 ]]; then
@@ -33,7 +30,7 @@ clear
 # RPM Fusion free and nonfree https://rpmfusion.org/Configuration/
 if dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y; then
-    echo -e "$(date +%T) finished installing RPM Fusion free and nonfree repositories" >> $logfile
+    echo -e "$(date +%T) installed RPM Fusion free and nonfree repositories" >> $logfile
 else
     echo -e "$(date +%T) ERROR (FATAL): failed installing RPM Fusion free and nonfree repositories - exiting" >> $logfile
     exit 1
@@ -41,7 +38,7 @@ fi
 
 # RPM Fusion tainted repos
 if dnf install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted -y; then
-    echo -e "$(date +%T) finished installing RPM Fusion free tainted and nonfree tainted repositories" >> $logfile
+    echo -e "$(date +%T) installed RPM Fusion free tainted and nonfree tainted repositories" >> $logfile
 else
     echo -e "$(date +%T) ERROR (FATAL): failed installing RPM Fusion free tainted and nonfree tainted repositories - exiting" >> $logfile
     exit 1
@@ -49,12 +46,28 @@ fi
 
 # Flatpak repo
 if flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; then
-    echo -e "$(date +%T) finished installing Flathub repository" >> $logfile
+    echo -e "$(date +%T) installed Flathub repository" >> $logfile
     flathub=true
 else
     echo -e "$(date +%T) ERROR: failed installing Flathub repository - continuing without it" >> $logfile
     flathub=false
 fi
+
+# Microsoft Visual Studio Code repo https://code.visualstudio.com/docs/setup/linux
+rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "$(date +%T) imported Microsoft signing key" >> $logfile
+
+if echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo; then
+    echo -e "$(date +%T) installed Visual Studio Code repository" >> $logfile
+else
+    echo -e "$(date +%T) ERROR: failed installing Visual Studio Code repository - continuing without it" >> $logfile
+fi
+
+
+#dnf check-update
+
+# Google Chrome repo
+
+
 
 
 ##### REMOVE UNWANTED PACKAGES
@@ -115,6 +128,10 @@ declare -a packages=(
     "wireshark"
 )
 
+declare -a group_packages=(
+    "--with-optional virtualization"
+)
+
 if [[ $nvidia = true ]]; then
     declare -a fusion_packages=(
         "fuse-exfat"
@@ -132,13 +149,16 @@ else
     )
 fi
 
-declare -a group_packages=(
-    "--with-optional virtualization"
+declare -a proprietary_packages=(
+    "code"
 )
 
 # installs all packages with one command
-dnf install $(echo ${packages[@]} ${fusion_packages[@]}) -y && \
-echo -e "$(date +%T) installed the following packages:\n$(for i in ${packages[@]}; do echo "  $i"; done)\n$(for i in ${fusion_packages[@]}; do echo "  $i"; done)" >> $logfile
+dnf install $(echo ${packages[@]} ${fusion_packages[@]} ${proprietary_packages[@]}) -y && \
+echo -e "$(date +%T) installed the following packages: 
+$(for i in ${packages[@]}; do echo "  $i"; done) \
+\n$(for i in ${fusion_packages[@]}; do echo "  $i"; done) \
+\n$(for i in ${proprietary_packages[@]}; do echo "  $i"; done)" >> $logfile
 
 # install group packages
 dnf groupinstall $(echo ${group_packages[@]}) -y && \
@@ -146,11 +166,8 @@ echo -e "$(date +%T) installed the following package groups:\n$(for i in "${grou
 
 # install Flatpaks, if the Flathub repo installed correctly
 if [[ $flathub = true ]]; then
-    # install Discord Flatpak
-    flatpak install flathub com.discordapp.Discord -y && echo -e "$(date +%T) finished installing Discord Flatpak" >> $logfile
-
-    # install Slack Flatpak
-    flatpak install flathub com.slack.Slack -y && echo -e "$(date +%T) finished installing Slack Flatpak" >> $logfile
+    flatpak install flathub com.discordapp.Discord -y && echo -e "$(date +%T) installed Discord Flatpak" >> $logfile
+    flatpak install flathub com.slack.Slack -y && echo -e "$(date +%T) installed Slack Flatpak" >> $logfile
 else
     echo -e "$(date +%T) could not install Discord Flatpak due to no Flathub repo" >> $logfile
     echo -e "$(date +%T) could not install Slack Flatpak due to no Flathub repo" >> $logfile
